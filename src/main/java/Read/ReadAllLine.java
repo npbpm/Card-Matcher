ef4791ce35451760e8707ef7934f1126180e29af
Mathilde <raby.m@outlook.fr>
2023-01-05 10:39:26 +0100

    lecture du CSV (tout le csv ligne par ligne)

---
+++ src/main/java/Read/ReadAllLine.java
@@ -0,0 +1,35 @@
+package Read;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.opencsv.CSVWriter;
+/**
+ * This class's purpose is to read all the lines. For now it doesn't do anything else, 
+ * but later it could be used to return a serie of mathing rates.
+ * The date should be written as a list of String. 
+ * @author rabym
+ *
+ */
+
+public class ReadAllLine {
+	public ReadAllLine (File file, String[] data) {
+		// create FileWriter object with file as parameter
+		FileWriter outputfile = null;
+		try {
+			Scanner sc = new Scanner(file);
+			sc.useDelimiter(",");
+			//setting comma as delimiter pattern
+		    while (sc.hasNext()) {
+		      //here we do something with sc.next()
+		    }
+		    //closes the scanner 
+		    sc.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
